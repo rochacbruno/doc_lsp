d58c30133e7321bbadf10c48071e8c81aba170de
Bruno Cesar Rocha <rochacbruno@users.noreply.github.com>
2025-08-18 19:51:03 +0100

    Create settings.py

---
+++ examples/settings.py
@@ -0,0 +1,20 @@
+"""Python configuration file,
+each variable is detailed with extensive details on
+settings.py.md
+"""
+
+SERVER = "localhost"
+PORT = 1234
+DEBUG = True
+ALLOW_RESOURCE_MANAGEMENT = True
+DEFAULT_ORG = "Acme"
+
+DATABASES = {
+  "default": {
+    "NAME": "foo",
+    "OPTIONS: {
+        "TLS_VERIFICATION": True,
+        "TIMEOUT": 30
+    }
+  }
+}
